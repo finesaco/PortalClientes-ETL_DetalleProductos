6a4a6744a43db3c8c37f5b44863a614cb57a3e72
Freddy Arteaga <freddyarteaga@finesa.com.co>
2023-11-22 17:18:12 -0500

    se ajustan rutas iniciales

--- Ejecuta_ETL_portal_detalle.sh
+++ Ejecuta_ETL_portal_detalle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'ejecuta etl'
 cd '/home/administrador/pentaho/data-integration/'
-sh '/home/administrador/pentaho/data-integration/kitchen.sh' -file:/home/products/DetalleProductos/Job_DetalleProductos.kjb " -param:CUENTA=PentahoBot"
+sh '/home/administrador/pentaho/data-integration/kitchen.sh' -file:/home/portal/PortalClientes-ETL_DetalleProductos/Job_DetalleProductos.kjb " -param:CUENTA=PentahoBot"
